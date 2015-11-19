99e804c5c04c10befea037f3f8997a1f0109c1fa
Wes Tarro <dudamoos@users.noreply.github.com>
2015-11-19 18:08:34 -0500

    In-class 3: Added PID control module (in development)

---
+++ pid.py
@@ -0,0 +1,59 @@
+import common
+import ach
+from time import time
+from math import *
+
+Input = ach.Channel(common.CAMERA_CHANNEL)
+Output = ach.Channel(common.DYNAMIXEL_CHANNEL)
+err = common.BallOffset()
+ServoPosition = common.ServoPosition()
+
+#-pi/4 to pi/4
+
+Kp = pi/4
+#Kd = 
+#Ki = 
+freq = 50.0
+period = 1/freq
+err_0 = [0.0, 0.0]
+err_i0 = [err_0[0]*period, err_0[1]*period]
+pos_0 = [0.0, 0.0]
+
+#xy: 0 for x 
+#xy: 1 for y
+
+def PID(err, xy):
+	theta_rp = err*Kp
+	#theta_rpd = theta_rp + Kd*(err-err_0[xy])/period
+	#err_i1 = err*period
+	#theta_rpid = theta_rpd + Ki*(err_i0[xy] + err_i1)
+	#err_i0[xy] += ei1
+	#err_0[xy] = err
+	return theta_rp#id
+
+def vel2pos(velos, xy):
+	return pos_0[xy] + velos*period
+
+while(True):
+	startTime = time() 
+	[status, framesize] = Input.get(err, wait=False, last=True)
+	if status != ach.ACH_OK and status != ach.ACH_STALE_FRAMES and status != ach.ACH_MISSED_FRAMES:
+		raise ach.AchException(Input.result_string())
+	
+	if(err.onscreen):
+		pos_x = vel2pos(PID(err.err[ERR_X], 0), 0)
+		pos_y = vel2pos(PID(err.err[ERR_Y], 1), 1)
+		pos_0 = [pos_x, pos_y]
+	else:
+		pos_x = vel2pos(0.5,0)
+		pos_y = vel2pos(0.5,1)
+		pos_0 = [pos_x, pos_y]
+	
+	ServoPosition.pos[common.POS_X] = pos_0[0]
+	ServoPosition.pos[common.POS_Y] = pos_0[1]
+	Output.put(ServoPosition)
+	
+	timeTaken = startTime + period - time()
+	if (timeTaken > 0):
+		sleep(timeTaken)
+
